3b6a8aec96a19317e24b06afee627e09524ba2f8
andree2403 <cjmorgan2589@gmail.com>
2022-09-11 14:55:55 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	for (; num < 10; num++)
+		putchar('0' + num);
+	putchar('\n');
+	return (0);
+}
