cdcbd86f2d7aaa959294374dcb713a9902570086
andree2403 <cjmorgan2589@gmail.com>
2022-09-16 16:02:19 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,29 +9,20 @@
 
 int main(void)
 {
-	int num = 1;
+	int num;
 
-	while (num++ <= 100)
+	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
+		if (num == 100)
+			printf("Buzz");
+		else if ((num % 3 == 0 && num % 5 == 0))
 			printf("FizzBuzz ");
-		}
 		else if ((num % 3) == 0)
-		{
 			printf("Fizz ");
-		}
 		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("%d ", num);
-			}
-		}
+			printf("Buzz ");
+		else
+			printf("%d ", num);
 	}
 	printf("\n");
 	return (0);
