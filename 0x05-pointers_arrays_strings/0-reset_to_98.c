4b2cc9b97394a28900a8bc35c904e37083d69080
andree2403 <cjmorgan2589@gmail.com>
2022-09-19 11:40:42 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the given integer to 98
+ * @n: parameter
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/reset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Result: 0
+ */
+
+int main(void)
+{
+	void reset_to_98(int *n);
+	return (0);
+}
