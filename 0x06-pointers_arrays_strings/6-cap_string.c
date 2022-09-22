ce172064f356dba31a37c110fe1b736e16d966e4
andree2403 <cjmorgan2589@gmail.com>
2022-09-22 01:53:07 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes the string
+ *
+ * @s: string
+ *
+ * Return: char value
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, delt = 'a' - 'A';
+	char seperators[13] = {'\n', '\t', ' ', '.', ',', ';', '"', '!', '?',
+	'(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == seperators[j])
+			{
+				if (s[i + 1] >= 'a' && s[i] <= 'z')
+					s[i + 1] -= delt;
+			}
+		}
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *cap_string(char *s);
+	return (0);
+}
