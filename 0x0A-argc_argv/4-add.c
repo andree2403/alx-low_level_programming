035abbf884591922b6cc759a89401362577f62a7
andree2403 <cjmorgan2589@gmail.com>
2022-09-30 14:34:12 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,12 +19,12 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (i != atoi(argv[i]))
+		if (!atoi(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		sum += _atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
