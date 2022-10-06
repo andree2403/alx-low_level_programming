88f17ed1deb789ae75c7536442a4c537d98af2a9
andree2403 <cjmorgan2589@gmail.com>
2022-10-06 14:08:37 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - calloc function
+ * @size: size of bytes
+ * @nmemb: number of elements
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *new;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	new = malloc(size * nmemb);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < size * nmemb; i++)
+		new[i] = 0;
+	return (new);
+}
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	char *string_nconcat(char *s1, char *s2, unsigned int n);
+	void *_calloc(unsigned int nmemb, unsigned int size);
 	return (0);
 }
