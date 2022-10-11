391975fc774b5332b2091a9083b46d4f6b701d79
andree2403 <cjmorgan2589@gmail.com>
2022-10-11 06:47:06 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
