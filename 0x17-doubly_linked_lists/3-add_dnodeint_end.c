5e1148d3af75c3a640c96bced830f04f5c4d3682
andree2403 <cjmorgan2589@gmail.com>
2023-01-27 14:35:56 +0000

    Tasks

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,10 +12,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *tp;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	tp = *head;
