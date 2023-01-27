2fbc5a5a9bd37cc8038c476d8b0cb78bf029b53e
andree2403 <cjmorgan2589@gmail.com>
2023-01-27 19:30:21 +0000

    Tasks

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all node data
+ * @head: head of node
+ * Return: sum of node data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
